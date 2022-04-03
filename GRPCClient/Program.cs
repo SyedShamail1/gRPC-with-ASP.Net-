1e9e2c3bd40ff64980e925c581bad1e22859d93a
SyedShamail1 <syedshamail3@gmail.com>
2022-04-03 15:17:46 +0500

    initial commit

---
+++ GRPCClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            #region GreeterService
+            //A Hello request
+            //var input = new HelloRequest
+            //{
+            //    Name = "Shamail"
+            //};
+
+            ////Setting up channel
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            ////Creating a client
+            //var client = new Greeter.GreeterClient(channel);
+
+            ////Getting a reply
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            //var input = new HelloRequest
+            //{
+            //Name = "Shamail"
+            //};
+            #endregion
+
+            #region Customer Service
+
+            var input = new CustomerLookupModel
+            {
+                UserId = 1
+            };
+
+            //Setting up channel
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            //Creating a client
+            var client = new Customer.CustomerClient(channel);
+
+            //Getting a reply
+            var reply = await client.GetCustomerInfoAsync(input);
+
+            Console.WriteLine(reply.FirstName + " " + reply.LastName);
+
+            #endregion
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if(request.UserId == 1)
+            {
+                output.FirstName = "Syed";
+                output.LastName = "Shamail";
+                output.IsAlive = true;
+            }
+            else if(request.UserId == 2)
+            {
+                output.FirstName = "Akber";
+                output.LastName = "Khan";
+                output.IsAlive = false;
+            }
+            else
+            {
+                output.FirstName = "Umer";
+                output.LastName = "Mushtaq";
+                output.IsAlive = false;
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
